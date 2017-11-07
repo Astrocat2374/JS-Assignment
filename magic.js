ea1b3cdf1c8d8f652d66cccd4fe39b7ac3082676
SilverNeko21 <larong2122@gmail.com>
2017-11-06 23:22:23 -1000

    only needs style

--- magic.js
+++ magic.js
@@ -1,5 +1,15 @@
-var F = 100
-function temperature() {
-	return C = (F-32) / 1.8 ;
+
+function temperature(F) {
+	return (F-32) / 1.8;
 }
-console.log(temperature())
+//console.log(temperature(32))
+
+for (i = 20; i <= 120; i += 5) {
+  console.log("Fahreinheight :" + i + " " + "Celcius :" + temperature(i));
+  var table = document.getElementById("Table");
+  var row = table.insertRow(-1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = i;
+  cell2.innerHTML = temperature(i);
+}
